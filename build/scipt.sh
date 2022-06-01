2bfeb88425806594294d3becdf50d5611e4f7458
prateek5916 <99383158+prateek5916@users.noreply.github.com>
2022-06-01 18:11:38 +0530

    Delete build directory

--- build/scipt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export GITHUB_MY_APP_DEPLOYMENT_ACCESS_TOKEN=asdfghjkl
-export BUILDKITE_COMMIT=qwertyuiop
-
- curl \
-        --header "Authorization: token $GITHUB_MY_APP_DEPLOYMENT_ACCESS_TOKEN" \
-        --request POST \
-        --data "{\"ref\": \"$BUILDKITE_COMMIT\"}" \
-        https://api.github.com/repos/my-org/my-app/deployments
