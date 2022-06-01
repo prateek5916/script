8f2573d43debe9a3a2aa9ab0a177e094319fcf22
prateek5916 <99383158+prateek5916@users.noreply.github.com>
2022-06-01 18:38:56 +0530

    Update scipt.sh

--- scipt.sh
+++ scipt.sh
@@ -5,6 +5,5 @@ export BUILDKITE_COMMIT=qsdcgyjn
 echo $ACCESS_TOKEN
  curl \
         --header "Authorization: token $ACCESS_TOKEN" \
-        --request POST \
         --data "{\"ref\": \"$BUILDKITE_COMMIT\"}" \
    
