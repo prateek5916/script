b77c34fa57cd164537ba06a484b5026f92f9ec0c
prateek5916 <99383158+prateek5916@users.noreply.github.com>
2022-06-01 18:22:45 +0530

    Update scipt.sh

--- scipt.sh
+++ scipt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export GITHUB_MY_APP_DEPLOYMENT_ACCESS_TOKEN=asdfghadksabdvjasdvashdjkl
+export ACCESS_TOKEN=asdfghadksabdvjasdvashdjkl
 export BUILDKITE_COMMIT=qwerasdbksahvdjdsajtyuiop
 
  curl \
-        --header "Authorization: token $GITHUB_MY_APP_DEPLOYMENT_ACCESS_TOKEN" \
+        --header "Authorization: token $ACCESS_TOKEN" \
         --request POST \
         --data "{\"ref\": \"$BUILDKITE_COMMIT\"}" \
         https://api.github.com/repos/my-org/my-app/deployments
