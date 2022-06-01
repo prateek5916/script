339572a3036e0fb4e00076edf50467258a4a2622
prateek5916 <99383158+prateek5916@users.noreply.github.com>
2022-06-01 18:35:06 +0530

    Update scipt.sh

--- scipt.sh
+++ scipt.sh
@@ -6,4 +6,4 @@ export BUILDKITE_COMMIT=qsdcgyjn
         --header "Authorization: token $ACCESS_TOKEN" \
         --request POST \
         --data "{\"ref\": \"$BUILDKITE_COMMIT\"}" \
-        https://api.github.com/repos/my-org/my-app/deployments
+   
